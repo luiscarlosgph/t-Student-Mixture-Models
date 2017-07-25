2fdd24e07b0ecdf0f57f924498b87d35af80b876
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2017-07-26 00:31:46 +0100

    Adapting __init__.py for Python3.3+.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 import unittest
 
 setuptools.setup(name='smm',
-    version='0.1.1',
+    version='0.1.4',
     description='t-Student-Mixture-Models',
     author='Luis C. Garcia-Peraza Herrera',
     author_email='luis.herrera.14@ucl.ac.uk',
@@ -14,22 +14,22 @@ setuptools.setup(name='smm',
     package_dir={'smm' : 'src/smm'}, 
     test_suite = 'tests',
     classifiers=[
-        'Intended Audience :: Developers'
-        'Intended Audience :: Science/Research'
-        'License :: OSI Approved'
-        'Operating System :: MacOS'
-        'Operating System :: Microsoft :: Windows'
-        'Operating System :: POSIX'
-        'Operating System :: Unix'
-        'Programming Language :: C'
-        'Programming Language :: Python'
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.6'
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.3'
-        'Programming Language :: Python :: 3.4'
-        'Topic :: Scientific/Engineering'
-        'Topic :: Software Development'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
          ],
 )
--- src/smm/__init__.py
+++ src/smm/__init__.py
@@ -1,2 +1,8 @@
-__version__ = '0.1.1'
-from smm import SMM
+import sys
+
+__version__ = '0.1.4'
+
+if (sys.version_info < (3, 3)):
+    from smm import SMM
+else:
+    from smm.smm import SMM
