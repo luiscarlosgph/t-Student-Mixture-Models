2af265a35ffc6868c205b367a3a313fc9e5b525c
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2017-07-25 22:54:05 +0100

    Classifiers added to setup.py

--- setup.py
+++ setup.py
@@ -18,4 +18,23 @@ setuptools.setup(name='smm',
     packages=['smm'],
     package_dir={'smm' : 'src/smm'}, 
     test_suite = 'tests',
+    classifiers=[
+        'Intended Audience :: Developers'
+        'Intended Audience :: Science/Research'
+        'License :: OSI Approved'
+        'Operating System :: MacOS'
+        'Operating System :: Microsoft :: Windows'
+        'Operating System :: POSIX'
+        'Operating System :: Unix'
+        'Programming Language :: C'
+        'Programming Language :: Python'
+        'Programming Language :: Python :: 2'
+        'Programming Language :: Python :: 2.6'
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4'
+        'Topic :: Scientific/Engineering'
+        'Topic :: Software Development'
+         ],
 )
