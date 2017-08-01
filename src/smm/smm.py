0cd943ebf7e9262b099b875849cc82b95ef485c0
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2017-08-01 13:24:37 +0100

    Adding spaces in documentation so that bullet points are displayed correctly. 4th try.

--- src/smm/smm.py
+++ src/smm/smm.py
@@ -1,6 +1,6 @@
 """t-Student Mixture Models Module (smm).
 
-- This module allows you to model data by a mixture of t-Student
+- This module allows you to model data by a mixture of t-Student \
 distributions, estimating the parameters with Expectation-Maximisation.
 It is an implementation of the paper: 'Robust mixture modelling using
 the t distribution', D. Peel and G. J. McLachlan. Published at:
@@ -10,8 +10,9 @@ Statistics and Computing (2000) 10, 339-348.
 
 - Compatible with Python 2.7 and Python 3.  
 
-- This code has been produced during my PhD at University College London
-under the supervision of Prof. Sebastien Ourselin and Dr. Tom Vercauteren.  
+- This code has been produced during my PhD at University College \
+London under the supervision of Prof. Sebastien Ourselin and Dr. Tom \
+Vercauteren.  
 
 Luis Carlos Garcia-Peraza Herrera (luis.herrera.14@ucl.ac.uk).
 24 Nov 2015.
