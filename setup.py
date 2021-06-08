ece892ede9d4292190376aff3949dafab47cc28a
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2021-06-08 02:37:20 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 import unittest
 
 setuptools.setup(name='smm',
-    version='0.1.0',
+    version='0.1.6',
     description='t-Student-Mixture-Models',
     author='Luis C. Garcia-Peraza Herrera',
     author_email='luis.herrera.14@ucl.ac.uk',
