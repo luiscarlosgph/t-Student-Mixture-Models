efa89ff278f465c98ff4346b68fe04f3f9b54a38
Luis Carlos Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2021-06-08 03:11:53 +0000

    Removing requirements file, now dependencies are in the setup file.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(name='smm',
     test_suite = 'tests',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires = ['numpy', 'sklearn', 'scipy'],
+    install_requires = ['numpy', 'scikit-learn', 'scipy', 'setuptools'],
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
